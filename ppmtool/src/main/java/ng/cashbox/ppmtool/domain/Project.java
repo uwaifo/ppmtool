34bb79e16d2c7049cef600155d70a12732414a0e
uwaifo <mr.uwaifo@gmail.com>
2020-03-02 22:23:24 +0100

    added domain object project attributes

--- ppmtool/src/main/java/ng/cashbox/ppmtool/domain/Project.java
+++ ppmtool/src/main/java/ng/cashbox/ppmtool/domain/Project.java
@@ -1,5 +1,107 @@
 package ng.cashbox.ppmtool.domain;
+
+import javax.persistence.*;
+import java.util.Date;
+
 //Created by Uwaifo Idehenre
+@Entity
 public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Float id;
+
+
+    private String projectName;
+    private String projectIdentifier;
+    private String projectDescription;
+
+    private Date startDate;
+    private Date endDate;
+
+    private Date createdAt;
+    private Date updatedAt;
+
+    //Constructor with no arguments
+    public Project() {
+    }
+
+    //Getter and setters
+
+
+    public Float getId() {
+        return id;
+    }
+
+    public void setId(Float id) {
+        this.id = id;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getProjectIdentifier() {
+        return projectIdentifier;
+    }
+
+    public void setProjectIdentifier(String projectIdentifier) {
+        this.projectIdentifier = projectIdentifier;
+    }
+
+    public String getProjectDescription() {
+        return projectDescription;
+    }
+
+    public void setProjectDescription(String projectDescription) {
+        this.projectDescription = projectDescription;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = new Date();
+
+    }
+
+    @PreUpdate
+    protected  void onUpdate() {
+        this.updatedAt = new Date();
+    }
 
 }
